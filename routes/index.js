90bdba38e11e126182b70a3e93cc81953fb769a2
r611171 <edward.cheadle@cambiahealth.com>
2017-08-11 21:28:27 -0600

    fixed index.js

--- routes/index.js
+++ routes/index.js
@@ -1,47 +1,20 @@
 module.exports.index = index;
-// module.exports.login = login;
+module.exports.login = login;
 module.exports.loginProcess = loginProcess;
-// module.exports.chat = chat;
+module.exports.chat = chat;
 
-// function index(req, res){
-//   res.render('index', {title: 'Index'});
-// };
-// exports.index = function index(req, res){
-//   res.render('index', {title: 'Index', cookie: JSON.stringify(req.
-//     cookies)});
-// };
-// exports.index = function index(req, res){
-//   res.cookie('IndexCookie', 'This was set from Index');
-//   res.render('index', {title: 'Index', cookie: JSON.stringify(req.
-//   cookies)});
-// };
-// function index(req, res){
-//   res.cookie('IndexCookie', 'This was set from Index');
-//   res.render('index', {title: 'Index', cookie: JSON.stringify(req.
-//   cookies), session: JSON.stringify(req.session)});
-// };
 function index(req, res){
-  // res.cookie('IndexCookie', 'This was set from Index');
-  res.render('index', {title: 'Index' });
-  // cookie: JSON.stringify(req.cookies),
-  // session: JSON.stringify(req.session),
-  // signedCookie: JSON.stringify(req.signedCookies)});
+  res.render('index', { title: 'Index' });
 };
 
-exports.login = function chat(req, res){
+function login(req, res){
   res.render('login', {title: 'Login'});
 };
 
-exports.chat = function chat(req, res){
-res.render('chat', {title: 'Chat'});
-};
-
-// function login(req, res){
-//   res.send('Login');
-// };
 function loginProcess(req, res){
   res.redirect('/');
 };
-// function chat(req, res){
-//   res.send('Chat');
-// };
+
+function chat(req, res){
+  res.render('chat', { title: 'Chat' });
+};
